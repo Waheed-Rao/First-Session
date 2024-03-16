aeeb30573c54c4c334fd278ac4caccba5b2966e1
Waheed-Rao <108606703+Waheed-Rao@users.noreply.github.com>
2024-03-16 12:45:13 +0500

    these are my first two files

---
+++ main.js
@@ -0,0 +1,4 @@
+console.log("hello world Pakistan");
+var myName = "Waheed";
+var message = "is a nice person";
+console.log(myName + " " + message);
---
+++ main.ts
@@ -0,0 +1,6 @@
+
+console.log ("hello world Pakistan");
+
+let myName = "Waheed";
+let message = "is a nice person";
+console.log (myName+" "+message);
